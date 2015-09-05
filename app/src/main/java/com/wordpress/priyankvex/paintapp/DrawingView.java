d78f9692edb5c9e9dbbb284d0cd6ceb411697af6
Priyank Verma <priyank.rip@gmail.com>
2015-09-05 14:53:43 +0530

    Instantiate drawing objects.

--- app/src/main/java/com/wordpress/priyankvex/paintapp/DrawingView.java
+++ app/src/main/java/com/wordpress/priyankvex/paintapp/DrawingView.java
@@ -1,6 +1,10 @@
 package com.wordpress.priyankvex.paintapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -10,6 +14,17 @@ import android.view.View;
  * Class which provides the view on which drawing takes place.
  */
 public class DrawingView extends View{
+
+    // To hold the path that will be drawn.
+    private Path drawPath;
+    // Paint object to draw drawPath and drawCanvas.
+    private Paint drawPaint, canvasPaint;
+    // initial color
+    private int paintColor = R.color.black;
+    // canvas on which drawing takes place.
+    private Canvas drawCanvas;
+    // canvas bitmap
+    private Bitmap canvasBitmap;
 
     public DrawingView(Context context, AttributeSet attrs){
         super(context, attrs);
@@ -21,6 +36,17 @@ public class DrawingView extends View{
      * One time initialization reduces resource consumption.
      */
     private void setUpDrawing(){
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        // Arbitrary brush stroke width.
+        drawPaint.setStrokeWidth(20);
+        // Making drawing smooth.
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
 
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
     }
 }
